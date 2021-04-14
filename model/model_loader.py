95bd4b8a2dd5646189d10678f1d20df8ea7bd538
CQUEEN-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2021-04-14 19:44:55 +0800

    Update model_loader.py

--- model/model_loader.py
+++ model/model_loader.py
@@ -1,24 +1,25 @@
 import my_model.RC_GAN as RC
 import my_model.FC_GAN as FC
 import torch
-
 class model_loader:
-    def __init__(self,model_type,load_path):
+    def __init__(self,model_type,d_load_path,g_load_path):
         self.model = model_type
-        self.load_path = load_path
+        self.d_load_path = d_load_path
+        self.g_load_path = g_load_path
 
     def discriminator(self):
         if self.model == 'FC_GAN':
             D = FC.discriminator()
         elif self.model == "RC_GAN":
             D = RC.discriminator()
-        D.load_state_dict(torch.load(self.load_path))
+        D.load_state_dict(torch.load(self.d_load_path))
         return D
-        
+
     def generator(self):
         if self.model == 'FC_GAN':
             G = FC.generator()
         elif self.model == "RC_GAN":
             G = RC.generator()
-        G.load_state_dict(torch.load(self.load_path))
+        G.load_state_dict(torch.load(self.g_load_path))
         return G
+

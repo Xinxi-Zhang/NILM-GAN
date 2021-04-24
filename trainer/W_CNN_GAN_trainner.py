61ccd072b393ee251bceed2dd3bba10644edbccd
CQUEEN-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2021-04-24 19:38:42 +0800

    Update and rename W_CAN_GAN_trainner.py to W_CNN_GAN_trainner.py

--- trainer/W_CNN_GAN_trainner.py
+++ trainer/W_CNN_GAN_trainner.py
@@ -1,5 +1,4 @@
-import torch
-import torch.nn as nn
+import numpy as np
 from torch.utils.data import DataLoader
 from Data_util.NILMDataset import NILMDataset
 import sys
@@ -16,30 +15,30 @@ if __name__ == '__main__':
     z_dimension = 32
     d_lr = 1e-4
     g_lr = 1e-4
-    start_epoch = 0
+    start_epoch = 40
     Lambda = 10
-    critic_steps = 5
-    mission = 'W-CNN-GAN-00-uk-house1-wm(ADAM)'
-    saver = saver(taskname=mission, flag=True)
+    critic_steps = 1
+    mission = 'mv'
+    saver = saver(taskname=mission, flag=False)
 
     # to load the data using the NILMDataset
     data = NILMDataset(r'D:\Research\NILM\dataset\uk_dale', 'wm', 'uk', window_size, houses=[1], mode=0)
     dataloader = DataLoader(data, batch_size=batch_size, shuffle=True)
     loader = model_loader(model_type='W_CNN_GAN',
-                          d_load_path=r'C:\Users\69037\Desktop\NILM\git\NILM-GAN\save\W-CNN-GAN-00-uk-house1-wm\discriminator\182.pkl',
-                          g_load_path=r'C:\Users\69037\Desktop\NILM\git\NILM-GAN\save\W-CNN-GAN-00-uk-house1-wm\generator\182.pkl')
+                          d_load_path=r'C:\Users\69037\Desktop\NILM\git\NILM-GAN\save\mv\discriminator\40.pkl',
+                          g_load_path=r'C:\Users\69037\Desktop\NILM\git\NILM-GAN\save\mv\generator\40.pkl')
 
     # define the discriminator and generator
-    D = discriminator()
-    G = generator()
+    D = loader.discriminator()
+    G = loader.generator()
     critic = utils()
     if torch.cuda.is_available():
         D = D.cuda()
         G = G.cuda()
 
     # Binary cross entropy loss and optimizer
-    d_optimizer = torch.optim.Adam(D.parameters(), lr=d_lr, betas=[0.5, 0.9])
-    g_optimizer = torch.optim.Adam(G.parameters(), lr=g_lr, betas=[0.5, 0.9])
+    d_optimizer = torch.optim.SGD(D.parameters(), lr=d_lr)
+    g_optimizer = torch.optim.SGD(G.parameters(), lr=g_lr)
 
     one = torch.tensor(1, dtype = torch.float)
     mone = one * -1
@@ -47,9 +46,18 @@ if __name__ == '__main__':
     mone = mone.cuda()
 
     for epoch in range(start_epoch, num_epoch):
+
+        real_scores_l = []
+        fake_scores_l = []
+        G_cost_l = []
+        D_cost_l = []
+        W_l = []
+
         for i, (real_load, real_label) in enumerate(dataloader):
             for p in D.parameters():
                 p.requires_grad = True
+            for p in G.parameters():
+                p.requires_grad = False
 
             if i % critic_steps == 0:
                 flag = True
@@ -86,9 +94,16 @@ if __name__ == '__main__':
             Wasserstein_D = real_scores - fake_scores
             d_optimizer.step()
 
+            fake_scores_l.append(float(fake_scores.data))
+            real_scores_l.append(float(real_scores.data))
+            D_cost_l.append(float(D_cost.data))
+            W_l.append(float(Wasserstein_D.data))
+
             if flag == True:
                 for p in D.parameters():
                     p.requires_grad = False  # to avoid computation
+                for p in G.parameters():
+                    p.requires_grad = True
                 G.zero_grad()
                 z = torch.rand(real_load.shape[0], z_dimension, window_size).cuda()
                 z = z.cuda()
@@ -101,13 +116,14 @@ if __name__ == '__main__':
                 fake_scores.backward()
                 G_cost = -fake_scores
                 g_optimizer.step()
+                G_cost_l.append(float(G_cost.data))
 
         f = open(saver.get_txt_path(), 'a')
 
         print('Epoch [{}/{}], d_loss: {:.6f}, g_loss: {:.6f} '
               'D real: {:.6f}, D fake: {:.6f}, Wass_cost: {:.6f}, D Step: {:.1f}'.format(
-            epoch + 1, num_epoch, D_cost.data, G_cost.data,
-            real_scores.data.mean(), fake_scores.data.mean(), Wasserstein_D.data.mean(), critic_steps), file=f,
+            epoch + 1, num_epoch, np.mean(D_cost_l), np.mean(G_cost_l),
+            np.mean(real_scores_l), np.mean(fake_scores_l), np.mean(W_l), critic_steps), file=f,
             flush=True)
 
         print('Epoch [{}/{}], d_loss: {:.6f}, g_loss: {:.6f} '
